a44db85a24ba6722a521fc82d1e008f751df7118
pratap <abhay@thesolarlabs.com>
2021-11-27 15:22:33 +0530

    fix(security): Updates socket protocol to wss

--- src/websocket.ts
+++ src/websocket.ts
@@ -2,7 +2,7 @@ const subscribe = {
   type: "subscribe",
 };
 
-export const ws = new WebSocket("ws://city-ws.herokuapp.com");
+export const ws = new WebSocket("wss://city-ws.herokuapp.com");
 ws.onopen = () => {
   ws.send(JSON.stringify(subscribe));
 };
